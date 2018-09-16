cedf6d8372a78b7a7671e5f24d0530f10df438a5
Jasonsey <2627866800@qq.com>
2018-09-16 19:10:39 +0800

    [update]提升代码可读性

---
+++ src/api/cs542/dataset.py
@@ -0,0 +1,67 @@
+from PIL import Image
+from pathlib import Path
+
+
+grid_x = 30
+grid_y = 30
+
+
+def init_path(paths: list):
+    for path in paths:
+        p = Path(path)
+        if not p.exists():
+            p.mkdir(parents=True)
+
+
+def load_images(ori_path1: Path):
+    goods = []
+    bads = []
+    paths = [ori_path1 / 'Good_License', ori_path1 / 'Bad_License']
+
+    print('begin loading images')
+    for path in paths:
+        for image in path.glob('*.jpg'):
+            if image.name == 'Good_License':
+                goods.append(Image.open(image).convert('RGB'))
+            else:
+                bads.append(Image.open(image).convert('RGB'))
+    print('finished loading images')
+    return goods, bads
+
+
+def crop_images(goods: list, bads: list, input_path: Path, clear_path: Path, blur_path: Path):
+    images = [goods, bads]
+    for i in len(images):
+        for ii in len(images[i]):
+            img = images[i][ii]
+            range_x = img.width // grid_x
+            range_y = img.height // grid_y
+            for x in range(range_x):
+                for y in range(range_y):
+                    bbox = (x * grid_x, y * grid_y, x * grid_x + grid_x, y * grid_y + grid_y)
+                    slice_bit = img.crop(bbox)
+                    if i == 0:
+                        path1 = Path(input_path) / ('clear_' + str(ii) + '_' + str(x) + '_' + str(y) + '.jpg')
+                        path2 = Path(clear_path) / ('clear_' + str(ii) + '_' + str(x) + '_' + str(y) + '.jpg')
+                    else:
+                        path1 = Path(input_path) / ('blur_' + str(ii) + '_' + str(x) + '_' + str(y) + '.jpg')
+                        path2 = Path(blur_path) / ('blur_' + str(ii) + '_' + str(x) + '_' + str(y) + '.jpg')
+                    slice_bit.save(path1, optimize=True, bits=6)
+                    slice_bit.save(path2, optimize=True, bits=6)
+
+
+def main():
+    ori_path = "../../../data/input/License/Train"
+
+    clear_path = "../../../data/output/cs542/train/clear/"
+    blur_path = "../../../data/output/cs542/train/blurred/"
+    input_path = "../../../data/output/cs542/train/input_data/"
+
+    init_path([clear_path, blur_path, input_path])
+
+    good_img, bad_img = load_images(Path(ori_path))
+    crop_images(good_img, bad_img, Path(input_path), Path(clear_path), Path(blur_path))
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/api/cs542/train.py
@@ -0,0 +1,154 @@
+import numpy as np
+import cv2
+import os.path
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras import callbacks
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.optimizers import Adam
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.convolutional import Convolution2D
+from keras.utils import np_utils
+from keras import backend as k
+from pathlib import Path
+from pprint import pprint
+from easydict import EasyDict as edict
+
+from .dataset import init_path
+
+
+k.set_image_dim_ordering('th')
+seed = 7
+np.random.seed(seed)
+num_classes = 2
+
+
+def load_dataset(path: Path, positive_label: bool=True):
+    img_data_list = []
+    for img_path in path.glob('*.jpg'):
+        input_img = cv2.imread(str(img_path))
+        input_img = np.swapaxes(input_img, 0, 2)
+        img_data_list.append(input_img)
+    img_data = np.array(img_data_list, dtype=np.float32)
+    img_data /= 255
+
+    num_of_samples = img_data.shape[0]
+    if positive_label:
+        labels = np.ones((num_of_samples, ), dtype=np.int64)
+        log = {'length of positive': len(labels)}
+    else:
+        labels = np.zeros((num_of_samples, ), dtype=np.int64)
+        log = {'length of negative': len(labels)}
+    pprint(log)
+    return img_data, labels
+
+
+def prepare_train_data(dataset_dict: edict):
+    labels = np.concatenate((dataset_dict.positive.labels, dataset_dict.negative.labels), axis=0)
+    img_data = np.concatenate((dataset_dict.positive.img_data, dataset_dict.negative.img_data), axis=0)
+    labels = np_utils.to_categorical(labels, num_classes)
+    x, y = shuffle(img_data, labels, random_state=2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+    return x_train, x_test, y_train, y_test, img_data
+
+
+def gen_model(input_shape):
+    # input_shape = img_data[0].shape
+    # print(input_shape, 'input_shape')
+
+    model = Sequential()
+    model.add(Convolution2D(96, 7, 7, input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Convolution2D(256, 5, 5))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+
+    ###########################
+    learning_rate = 0.001
+    adam = Adam(lr=learning_rate)
+    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=["accuracy"])
+
+    model.summary()
+    model.get_config()
+    model.layers[0].get_config()
+    model.layers[0].input_shape
+    model.layers[0].output_shape
+    model.layers[0].get_weights()
+    np.shape(model.layers[0].get_weights()[0])
+    model.layers[0].trainable
+
+    return model
+
+
+def main():
+    blur_directory = '../../../data/output/cs542/train/blur/'
+    clear_directory = '../../../data/output/cs542/train/clear/'
+    filepath = "../../../data/output/cs542/models/"
+    init_path([filepath])
+
+    img_data_positive, labels_positive = load_dataset(Path(blur_directory), positive_label=True)
+    img_data_negative, labels_negative = load_dataset(Path(clear_directory), positive_label=False)
+    dataset_dict = edict({
+        'positive': {
+            'img_data': img_data_positive,
+            'labels': labels_positive
+        },
+        'negative': {
+            'img_data': img_data_negative,
+            'label': labels_negative
+        }
+    })
+
+    x_train, x_test, y_train, y_test, img_data = prepare_train_data(dataset_dict)
+
+    model = gen_model()
+    model.fit(x_train, y_train, batch_size=128, nb_epoch=100, verbose=1, validation_data=(x_test, y_test))
+
+    filename = 'model_train_new.csv'
+    csv_log = callbacks.CSVLogger(filename, separator=',', append=False)
+
+    early_stopping = callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='min')
+
+    # filepath="Best-weights-my_model-{epoch:03d}-{loss:.4f}-{acc:.4f}.hdf5"
+
+    checkpoint = callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+
+    # tensorboard callback
+    tensorboard_callback = callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True,
+                                                 write_grads=False, write_images=False, embeddings_freq=0,
+                                                 embeddings_layer_names=None, embeddings_metadata=None)
+
+    callbacks_list = [csv_log, early_stopping, checkpoint, tensorboard_callback]
+
+    # Evaluating the model
+
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print('Test Loss:', score[0])
+    print('Test accuracy:', score[1])
+
+    test_image = x_test[0:1]
+    print(test_image.shape)
+
+    print(model.predict(test_image))
+    print(model.predict_classes(test_image))
+    print(y_test[0:1])
+
+    # Save our model here
+    file = open(filepath + "motionblur.h5", 'a')
+    model.save(filepath + "motionblur.h5")
+    file.close()
+
+
+if __name__ == '__main__':
+    main()
