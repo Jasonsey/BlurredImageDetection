945dbccc0d3ba13c013b51d51dfd474f555c74af
Jasonsey <2627866800@qq.com>
2019-04-04 15:55:24 +0800

    【update】 add README.md and the trained model

---
+++ src/__main__.py
@@ -0,0 +1,12 @@
+# Bluerred Image Detection
+# 
+# Author: Jasonsey
+# Email: 2627866800@qq.com
+# 
+# =============================================================================
+"""main package entry"""
+from .main import main
+
+
+main()
+
